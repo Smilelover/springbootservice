0bb574f985e7a332af74aa7fba3436eec5273e77
chenjianeng <chenjianeng@jia-fu.cn>
2020-09-04 13:46:16 +0800

    fix

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+/**
+ * @EnableCaching  开启cache缓存
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableCaching
 public class DemoApplication {
 
     public static void main(String[] args) {
--- demo/src/main/java/com/example/demo/Service/GoodsService.java
+++ demo/src/main/java/com/example/demo/Service/GoodsService.java
@@ -1,8 +1,10 @@
 package com.example.demo.Service;
 
+import com.alibaba.fastjson.JSON;
 import com.example.demo.entity.Goods;
 import com.example.demo.mapper.GoodsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,7 +17,13 @@ public class GoodsService {
     @Autowired
     private GoodsMapper goodsMapper;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     public Goods selectGoods(){
-        return goodsMapper.selectByPrimaryKey((long) 1);
+        Goods goods = goodsMapper.selectByPrimaryKey((long)1);
+        redisTemplate.opsForValue().set("goods", JSON.toJSONString(goods));
+        return goods;
+
     }
 }
